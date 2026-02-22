d5a931fa107ca953474f8a0d7a0bcad5e8828759
SV MAIN <studentversedubai@gmail.com>
2026-02-22 23:22:39 +0400

    Made the code more scalable

--- Project 2.cpp
+++ Project 2.cpp
@@ -4,47 +4,77 @@
 #include <chrono>
 #include <thread>
 #include <cstdint>
+#include <vector>
 
-uint64_t FromFileTime(const FILETIME ft) {
+uint64_t FileTimeToInt64(const FILETIME ft) {
     ULARGE_INTEGER uli = { 0 };
     uli.LowPart = ft.dwLowDateTime;
     uli.HighPart = ft.dwHighDateTime;
     return uli.QuadPart;
 }
 
+struct FILETIME_as_int
+{
+    uint64_t idle;
+    uint64_t kernel;
+    uint64_t user;
+};
 
-uint64_t getSysTime() {
+struct cpuDeltas
+{
+    uint64_t dIdle;
+    uint64_t dKernel;
+    uint64_t dUser;
+    uint64_t dSystem;
+};
+
+FILETIME_as_int systemData() {
+
+    FILETIME_as_int fi;
     FILETIME idle, kernel, user;
-    FILETIME idle_2, kernel_2, user_2;
     GetSystemTimes(&idle, &kernel, &user);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    GetSystemTimes(&idle_2, &kernel_2, &user_2);
+    fi.idle = FileTimeToInt64(idle);
+    fi.kernel = FileTimeToInt64(kernel);
+    fi.user = FileTimeToInt64(user);
 
-    uint64_t idle0 = FromFileTime(idle);
-    uint64_t idle1 = FromFileTime(idle_2);
-    uint64_t kernel0 = FromFileTime(kernel);
-    uint64_t kernel1 = FromFileTime(kernel_2);
-    uint64_t user0 = FromFileTime(user);
-    uint64_t user1 = FromFileTime(user_2);
+    if (!GetSystemTimes(&idle, &kernel, &user)) return FILETIME_as_int{ 0,0,0 };
+    return fi;
+}
 
-    uint64_t totalIdle = idle1 - idle0;
-    uint64_t totalKernel = kernel1 - kernel0;
-    uint64_t totalUser = user1 - user0;
+cpuDeltas systemDataToInt(FILETIME_as_int previous, FILETIME_as_int current) {
 
-    uint64_t totalSys = totalKernel + totalUser;
+    cpuDeltas dv;
+    dv.dIdle = current.idle - previous.idle;
+    dv.dKernel = current.kernel - previous.kernel;
+    dv.dUser = current.user - previous.user;
+    dv.dSystem = dv.dKernel + dv.dUser;
+    
+    return dv;
+}
 
-    std::cout << "System Idle : " << totalIdle << std::endl;
-    std::cout << "System Kernel : " << totalKernel << std::endl;
-    std::cout << "System User : " << totalUser << std::endl;
-
-    //uint64_t cpu = (totalSys - totalIdle) * 100 / (totalSys);
-    return 0;
+void printSystemData(cpuDeltas dv) {
+    std::cout << "Idle : " << dv.dIdle << std::endl;
+    std::cout << "Kernel : " << dv.dKernel << std::endl;
+    std::cout << "User : " << dv.dUser << std::endl;
+    uint64_t cpu_percent = (dv.dSystem - dv.dIdle) * 100 / dv.dSystem;
+    std::cout << "System : " << cpu_percent << "%" << std::endl;
 }
 
 
 int main()
 {
-    getSysTime();
-    //std::cout << "Raw CPU usage : " << getSysTime() << std::endl;
+    FILETIME_as_int first_run = systemData();
+    int cpu_runs = 10;
+    for(int i{0}; i < cpu_runs; i++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        FILETIME_as_int second_run = systemData();
+
+        cpuDeltas deltas = systemDataToInt(first_run, second_run);
+        system("cls");
+        printSystemData(deltas);
+        first_run = second_run;
+    }
+    
 }
 
